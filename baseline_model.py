c2863d420b16628af5923fe5856fe9c71fa00adf
Arwa Raies <46404792+arwaraies@users.noreply.github.com>
2020-02-28 16:16:05 +0000

    Update baseline_model.py

--- baseline_model.py
+++ baseline_model.py
@@ -112,18 +112,6 @@ def sample_negative_edges_other_ideas(G, test_edges):
             new_node = choice(tuple(possible_nodes))
             neg_edges.add((lhs,new_node))
             G.add_edge(lhs,new_node)
-           
-        #new_node = choice(possible_nodes)
-        #sample_size = len(possible_nodes)
-        #count = 0
-        
-        #while node_types[new_node] != rhs_type and count < sample_size:
-        #    new_node = choice(possible_nodes)
-        #    count+=1
-            
-        #neg_edges.append((lhs,new_node))
-        
-        #G.add_edge(lhs,new_node)
 
     G.remove_edges_from(neg_edges)
     
